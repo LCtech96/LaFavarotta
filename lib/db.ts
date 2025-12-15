65a77b0eb9038d8806cd13c55ae64c3768d43de9
LCtech96 <lucacorrao96@outlook.it>
2025-12-15 21:22:02 +0100

    Make Prisma client optional to prevent build errors

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,24 @@
-import { PrismaClient } from '@prisma/client'
+// Prisma Client - will be generated during build
+// This file is optional and only needed if you want to use the database
+// Currently, menu data is static in data/menu-data.ts
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+let PrismaClient: any
+try {
+  PrismaClient = require('@prisma/client').PrismaClient
+} catch (e) {
+  // Prisma Client not generated yet - this is OK for static data
+  PrismaClient = null
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+const globalForPrisma = globalThis as unknown as {
+  prisma: any | undefined
+}
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export const prisma = PrismaClient
+  ? globalForPrisma.prisma ?? new PrismaClient()
+  : null
+
+if (process.env.NODE_ENV !== 'production' && prisma) {
+  globalForPrisma.prisma = prisma
+}
 
