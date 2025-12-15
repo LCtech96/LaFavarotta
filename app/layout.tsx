0f8bbcd166074199bb05d221d198482186b68f84
LCtech96 <lucacorrao96@outlook.it>
2025-12-15 22:57:22 +0100

    Add favicon to the site

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,10 @@ import { ThemeProvider } from "@/components/theme-provider";
 export const metadata: Metadata = {
   title: "La Favarotta - Ristorante Pizzeria Terrasini",
   description: "Ristorante Pizzeria sala banchetti La Favarotta di Leone Vincenzo & cS.S. 113 Terrasini (PA)",
+  icons: {
+    icon: '/favicon-image.png',
+    apple: '/favicon-image.png',
+  },
 };
 
 export default function RootLayout({
